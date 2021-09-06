143c115cb8bbff5d6d563f26edcb684d301264b3
artemvolkovrarog <temichv@rambler.ru>
2021-09-06 15:50:19 +0300

    Message implementation and file with prefixes added.

--- src/main/java/com/db/edu/team05/utilities/Commands.java
+++ src/main/java/com/db/edu/team05/utilities/Commands.java
@@ -3,24 +3,24 @@ package com.db.edu.team05.utilities;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Prefix {
+public class Commands {
     public static final String SEND = "snd";
 
     public static final String HISTORY = "hist";
 
     public static final String CHILD = "chid";
 
-    private static Collection<String> prefixes = new ArrayList<>();
+    private static Collection<String> commands = new ArrayList<>();
 
     static {
-        prefixes.add(SEND);
-        prefixes.add(HISTORY);
-        prefixes.add(CHILD);
+        commands.add(SEND);
+        commands.add(HISTORY);
+        commands.add(CHILD);
 
     }
 
-    public static boolean isPrefix(String s){
-        return prefixes.contains(s);
+    public static boolean isCommand(String s){
+        return commands.contains(s);
     }
 
 
