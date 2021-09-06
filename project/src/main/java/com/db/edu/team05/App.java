8c80380e253970c0d2eedb9e55d829ae17beb356
Ekaterina Kuzina <ekaterina.kuzina2@yandex.ru>
2021-09-06 10:13:50 +0300

    Maven project created. README added.

---
+++ project/src/main/java/com/db/edu/team05/App.java
@@ -0,0 +1,13 @@
+package com.db.edu.team05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
