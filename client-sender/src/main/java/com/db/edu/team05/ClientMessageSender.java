2a14311ba73916dea0b70ba857af1ffbf7f4a861
Ekaterina Kuzina <ekaterina.kuzina2@yandex.ru>
2021-09-07 18:51:53 +0300

    Client message charset debugged.

--- client-sender/src/main/java/com/db/edu/team05/ClientMessageSender.java
+++ client-sender/src/main/java/com/db/edu/team05/ClientMessageSender.java
@@ -2,6 +2,7 @@ package com.db.edu.team05;
 
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class ClientMessageSender {
@@ -18,7 +19,9 @@ public class ClientMessageSender {
         while (true) {
             String inputString = new Scanner(System.in).nextLine();
             if (CorrectnessChecker.checkIfMessageCorrect(inputString)) {
-                connector.getOutput().write(inputString);
+                byte[] bytes = inputString.getBytes();
+                String inputUTF8 = new String(bytes,StandardCharsets.UTF_8);
+                connector.getOutput().write(inputUTF8);
                 connector.getOutput().flush();
             } else System.out.println("Incorrect message! Try again:(");
 
